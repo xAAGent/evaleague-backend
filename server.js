ef2c0dec1065642c4aac474590bdd00737812fe3
Z4Zachary <95265170+Z4Zachary@users.noreply.github.com>
2025-02-05 17:17:57 -0600

    Update backend to use DigitalOcean's default port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const PASSWORD = "LeagueTeam2025";
 
 // Middleware
